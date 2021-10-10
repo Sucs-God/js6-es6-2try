3f6674e6d4c6cc20484d952c9c6063d5c86f97ea
Sucs-God <technogen228@gmail.com>
2021-10-10 15:16:07 +0300

    [code] 1 Part let and const

---
+++ js/1_let_const.js
@@ -0,0 +1,50 @@
+// var num = 42
+
+// num = 'str'
+
+// console.log(num)
+
+// let a = 24
+
+// if (true) {
+//     let a = 42
+//
+// console.log('a', a)
+// }
+
+// console.log(a)
+
+// Hosting
+
+// b = 20
+//
+// console.log(b)
+//
+// let b = 10
+
+// function hoisted() {
+//     age = 26
+// }
+//
+// let age
+// hoisted()
+// console.log(age)
+
+// Const
+const COLOR = '#ffeebb'
+// COLOR = '#000' // Ошибка
+//
+// console.log(COLOR)
+//
+// const array = [1, 2, 3, 5, 8] // массив можно менять внутреннее состояние массива, но нельзя переназначать
+//
+// console.log(array)
+//
+// array.push(13)
+//
+// console.log(array)
+
+const obj = {a: 42}// так же как и со списком
+obj.name = 'Igor'
+
+console.log(obj)
