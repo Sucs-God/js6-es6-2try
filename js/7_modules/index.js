943bdc10255d3d7e1bdd17798d7dd949a3b5e695
Sucs-God <technogen228@gmail.com>
2021-10-23 17:23:48 +0300

    [code] 7 Part Modules

---
+++ js/7_modules/index.js
@@ -0,0 +1,10 @@
+// import MyLog, {COLOR, compute} from './module'
+import * as Module from './module'
+
+// compute(1, 2)
+// console.log(COLOR)
+// MyLog.log()
+
+Module.compute(1, 2)
+Module.COLOR
+Module.default.log()
